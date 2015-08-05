2f152c399eb3cba74aa7dcc784f5088d8fecdda6
ssut <ssut@ssut.me>
2015-08-06 01:24:28 +0900

    추가: 로그인 URL 및 정규식

--- clien/constants.py
+++ clien/constants.py
@@ -3,6 +3,18 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import re
 
 #: 클리앙 모바일 기본 URL
 CLIENM_URL = 'http://m.clien.net/cs3'
+
+class CLIENM_URI(object):
+    """클리앙 URI들"""
+
+    SIGNIN = 'https://www.clien.net/cs2/bbs/login_check.php'
+
+
+class REGEXP(object):
+    """정규식"""
+
+    ALERT_CONTENT = re.compile(r'alert\(\'?\"?([^\"\']+)', re.MULTILINE)
