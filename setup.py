179c07c01af09615d78aa6da133b265a993f495f
ssut <ssut@ssut.me>
2015-08-06 00:04:10 +0900

    Add: setup.py (proto)

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
