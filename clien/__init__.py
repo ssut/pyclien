7159bec4d8e51f2fc1f2ab39133aaebd4a660b5b
ssut <ssut@ssut.me>
2015-08-06 00:07:40 +0900

    Add: base module

---
+++ clien/__init__.py
@@ -0,0 +1,14 @@
+"""clien
+   ~~~~~
+
+   Python용 비공식 클리앙(clien.net) 라이브러리
+
+   :copyright: (c) 2015 by SuHun Han (ssut).
+   :license: MIT License, see LICENSE for more details.
+"""
+
+__author__ = 'ssut'
+__author_email__ = 'ssut@ssut.me'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__url__ = 'https://github.com/ssut/pyclien'
