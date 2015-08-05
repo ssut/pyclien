74c05f97d84c2af11ebef56df3a0a6e042bac89c
ssut <ssut@ssut.me>
2015-08-06 00:30:59 +0900

    Add: classes

---
+++ clien/classes.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+""":mod:`clien.classes` --- Classes for CLIEN
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+class ClienProfile(object):
+    pass
+
+
+class ClienArticle(object):
+    pass
+
+
+class ClienComment(object):
+    pass
+
+
+class ClienMessage(object):
+    pass
