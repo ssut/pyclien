4fc422091979c1d8f11b7adce63d9ad23517e327
ssut <ssut@ssut.me>
2015-08-06 09:21:38 +0900

    추가: 로그인 예제

--- example/basic.py
+++ example/basic.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+from __future__ import print_function
 import os
 import sys
 
@@ -7,5 +9,19 @@ except:
     sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     from clien import Session
 
-clien = Session()
-print(clien)
+def main():
+    clien = Session()
+    success = False
+    if not os.path.exists('session.tmp'):
+        success, reason = clien.login(username='', password='')
+        if success:
+            clien.save_session('session.tmp')
+        else:
+            print(reason)
+    else:
+        success, reason = clien.login(sessionpath='session.tmp')
+    print(u'로그인 결과:', success)
+
+
+if __name__ == '__main__':
+    main()
