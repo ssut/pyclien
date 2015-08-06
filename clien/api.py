b1b2024cef2835b586914bd1f209a698ece9543a
ssut <ssut@ssut.me>
2015-08-06 09:21:23 +0900

    추가: 로그인 구현 및 세션 저장 지원

--- clien/api.py
+++ clien/api.py
@@ -3,6 +3,15 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import os
+import pickle
+
+import requests
+from bs4 import BeautifulSoup
+
+from clien.constants import CLIENM_URL, CLIENM_URI
+from clien.constants import REGEXP
+from clien.dev import report
 
 class ClienAPI(object):
     """클리앙 API 클래스.
@@ -10,3 +19,71 @@ class ClienAPI(object):
 
     def __init__(self, *args, **kwargs):
         raise NotImplemented(u'ClienAPI 클래스는 Session 클래스에 상속되어야 합니다.')
+
+    def login(self, username=None, password=None, sessionpath=None):
+        """클리앙에 로그인합니다.
+        아이디 + 비밀번호 조합으로 넘기거나 세션 파일을 미리 저장해둔 경우 세션 파일 경로를 넘기면 됩니다.
+
+        :param str username: 클리앙 아이디
+        :param str password: 클리앙 비밀번호
+        :param str sessionpath: (저장한 세션이 존재하는 경우) 세션 파일 경로"""
+        session = None
+        result = False
+        reason = ''
+
+        if sessionpath:
+            if not os.path.exists(sessionpath):
+                raise IOError(u'존재하지 않는 세션 파일입니다: '.format(sessionpath))
+            with open(sessionpath, 'r') as f:
+                cookies = pickle.load(f)
+            if not cookies:
+                raise IOError(u'유효하지 않은 세션 파일입니다: '.format(sessionpath))
+            self.session = requests.Session()
+            self.session.cookies = cookies
+            result = self.check_logged()
+
+        if username and password:
+            session = requests.Session()
+            payload = {
+                'url': CLIENM_URL,
+                'mb_id': username,
+                'mb_password': password,
+            }
+            r = session.post(CLIENM_URI.SIGNIN, data=payload)
+            r.encoding = 'utf-8'
+            if 'history.go(-1);' in r.text:
+                result = False
+                reason = REGEXP.ALERT_CONTENT.search(r.text).groups()[0]
+            else:
+                result = True
+            result = self.check_logged(r.text)
+            self.session = session
+
+        return (result, reason, )
+
+    def check_logged(self, content=None):
+        """로그인 여부를 확인합니다.
+
+        :param str content: 미리 요청한 리퀘스트가 존재하는 경우 이 파라미터로 전달
+        """
+        result = False
+
+        if not content:
+            r = self.session.get(CLIENM_URL)
+            r.encoding = 'utf-8'
+            content = r.text
+        if '?nowlogin=1' in content:
+            result = True
+        else:
+            content = BeautifulSoup(content, 'lxml')
+            try:
+                btntext = content.select('header h1 + button')[0].text
+            except IndexError:
+                report(content)
+                raise ValueError(u'로그인 상태를 확인할 웹 객체를 찾지 못했습니다.')
+            if btntext == u'로그인':
+                result = False
+            elif btntext == u'로그아웃':
+                result = True
+
+        return result
--- clien/session.py
+++ clien/session.py
@@ -3,6 +3,9 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import os
+import pickle
+
 import requests
 
 from clien.api import ClienAPI
@@ -13,6 +16,18 @@ class Session(ClienAPI):
     :class:`requests.Session`를 이용해 세션을 유지하며 모든 API를 포함합니다."""
 
     def __init__(self, username=None, password=None, sessionpath=None):
+        self.session = requests.Session()
         if not sessionpath and username and password:
-            pass
+            success, reason = self.login(username=username, password=password)
+            print(reason)
 
+    def save_session(self, path):
+        """세션 정보를 저장합니다.
+
+        :param str path: 저장할 경로
+        :return: 저장 성공여부"""
+        with open(path, 'w') as f:
+            print(self.session.cookies)
+            pickle.dump(self.session.cookies, f)
+        result = os.path.exists(path)
+        return result
